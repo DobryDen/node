939afaf1c40fa915149911514f7fd666d0185d24
Denis Smolyaninov <den.hgmt@gmail.com>
2017-02-09 19:51:00 +0200

    4 lesson

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 
 var app = express();
+var db;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-var artists = [
+var artists = 
+[
 { 
 	id: 1,
 	name: 'Metallica'
@@ -21,36 +25,55 @@ var artists = [
 }
 ];
 
-app.get('/', function (req, res){
+app.get('/', function (req, res) {
 	res.send('Hello API');
 	
 })
 
-app.listen(3012, function(){
-	console.log('API app started');
+app.get('/artists', function (req, res) {
+	db.collection('artists').find().toArray(function(err,docs){
+		if(err){
+			console.log(err);
+			return res.sendStatus(500);
+		}
+		res.send(docs);
+	})
+		
 	
 })
 
-app.get('/artists', function (req, res){
-	res.send(artists);
-} )
+app.get('/artists/:id', function (req, res) { 
+	db.collection('artists').findOne({_id: ObjectID(req.params.id)},function(err,doc){
+		if (err){
+			console.log(err);
+			return res.sendStatus(500);
+		}
+		res.send(doc);
+	})
 
-app.get('/artists/:id', function (req, res){
-	console.log(req.params);
-	var artist = artists.find(function(artist){
-		return artist.id === Number(req.params.id)
-	});
-	res.send(artist);
+	//console.log(req.params);
+	//var artist = artists.find(function(artist) {
+	//	return artist.id === Number(req.params.id)
+	//});
+	//res.send(artist);
 	
 
 })
 app.post('/artists', function (req, res) {
 	var artist = {
-		id: Date.now(),
+	
 		name: req.body.name
 	};
-	artists.push(artist);
-	res.send(artist);
+	db.collection('artists').insert(artist, function(err, result) {
+		if (err) {
+			console.log(err);
+			return res.sendStatus(500);
+		}
+		res.send(artist);
+	})
+
+
+	//res.send(artist);
 
 	//console.log(req.body);
 	//res.send('post data');
@@ -67,4 +90,13 @@ app.delete('/artists/:id', function (req, res){
 		return artist.id !== Number(req.params.id);
 	})
 	res.sendStatus(200);
+})
+MongoClient.connect('mongodb://localhost:27017/myapi', function(err, database){
+	if (err){
+		return console.log(err);
+	}
+	db = database;
+	app.listen(3012, function(){
+		console.log('API app started');
+	})
 })
